3292f9981e87ea4a0464656d5e0ef2870f9e76f1
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2022-08-22 22:34:06 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+#define SSID "ESP32Server"
+#define PASSWORD "12345678"
+#define SERVER_PORT 80
+
+WiFiServer server(SERVER_PORT);
+
+int temperature = 24;
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(SSID, PASSWORD);
+
+  IPAddress ip = WiFi.softAPIP();
+  Serial.println(ip);
+
+  server.begin();
+}
+
+void loop()
+{
+  WiFiClient client = server.available();
+
+  if (client)
+  {
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        char c = client.read();
+        Serial.write(c);
+      }
+    }
+
+    client.stop();
+  }
+}
